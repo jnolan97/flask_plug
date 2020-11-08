3ccfe7b537bcce6371171554d617412cb2d93c4c
John Nolan <jnolan@healthadvocate.net>
2020-11-08 13:31:19 -0500

    navbar and pages

--- venv/routes.py
+++ venv/routes.py
@@ -4,3 +4,11 @@ from flask import render_template, request, redirect, url_for
 @app.route('/')
 def base():
     return render_template("base.html")
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    return redirect(url_for('base'))
+
+@app.route('/smartplug', methods=['GET','POST'])
+def smartplug():
+    return render_template("plug.html")
